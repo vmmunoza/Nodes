1a29cf5f08b192f00ae3da2681325547fa0ad8fa
Víctor Muñoz <53861440+vmmunoza@users.noreply.github.com>
2023-06-22 19:17:59 +0200

    Update NFT.sol
    The simplest form of an NFT contract. It only allows new tokens to be minted. You can add metadata to the token, which could be done by overriding the `_baseURI()` function from ERC721.

--- NFT.sol
+++ NFT.sol
@@ -1,6 +1,3 @@
-Sure, here's an example of a simple NFT contract, written in Solidity, and leveraging the ERC721 standard from OpenZeppelin. I'll comment each part to clarify its purpose:
-
-```solidity
 // SPDX-License-Identifier: MIT
 // Define the version of Solidity that will be used to compile this contract
 pragma solidity ^0.8.0;
@@ -25,14 +22,3 @@ contract MyNFT is ERC721 {
         _mint(to, nextTokenId);
     }
 }
-```
-
-This is the simplest form of an NFT contract. It only allows new tokens to be minted. In a real-world use case, you would probably want to include some sort of access control to restrict who can mint new tokens. Also, depending on your use case, you might want to add metadata to your tokens, which could be done by overriding the `_baseURI()` function from ERC721.
-
-This contract assumes that the Solidity compiler version is 0.8.0 or higher, and that the OpenZeppelin contracts have been installed in your project. If not, you can install them using npm:
-
-```bash
-npm install @openzeppelin/contracts
-```
-
-The SPDX-License-Identifier: MIT at the top of the contract is a license identifier, which helps developers to know they can use this contract under the terms of the MIT license.
